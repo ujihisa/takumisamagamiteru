6f86104a07a27654d238f25b812e97e5036a3823
ujihisa <ujihisa@gmail.com>
2012-02-29 22:28:32 -0800

    first commit

---
+++ src/takumisamagamiteru/core.clj
@@ -0,0 +1,1 @@
+(ns takumisamagamiteru.core)
---
+++ test/takumisamagamiteru/test/core.clj
@@ -0,0 +1,6 @@
+(ns takumisamagamiteru.test.core
+  (:use [takumisamagamiteru.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
