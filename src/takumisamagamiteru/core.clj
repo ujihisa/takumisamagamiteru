671996e9056f7067be60676000376fb8a56827a8
ujihisa <ujihisa@gmail.com>
2012-03-04 00:06:21 -0800

    for 1.2

--- src/takumisamagamiteru/core.clj
+++ src/takumisamagamiteru/core.clj
@@ -1,7 +1,7 @@
 (ns takumisamagamiteru.core
-  (:require [cljminecraft.core :as c])
+  (:require [swank.swank])
+  (:require [clojure.string :as s])
   (:import [org.bukkit Bukkit])
-  (:import [org.bukkit.event Event Event$Type])
   (:import [org.bukkit.entity Animals Arrow Blaze Boat CaveSpider Chicken
             ComplexEntityPart ComplexLivingEntity Cow Creature Creeper Egg
             EnderCrystal EnderDragon EnderDragonPart Enderman EnderPearl
@@ -13,50 +13,32 @@
             ThrownPotion TNTPrimed Vehicle Villager WaterMob Weather Wolf
             Zombie])
   (:import [org.bukkit.event.entity CreatureSpawnEvent CreeperPowerEvent
-            EndermanPickupEvent EndermanPlaceEvent EntityChangeBlockEvent
+            EntityChangeBlockEvent
             EntityCombustByBlockEvent EntityCombustByEntityEvent
             EntityCombustEvent EntityCreatePortalEvent EntityDamageByBlockEvent
-            EntityDamageByEntityEvent EntityDamageByProjectileEvent
+            EntityDamageByEntityEvent
             EntityDamageEvent EntityDeathEvent EntityEvent EntityExplodeEvent
             EntityDamageEvent$DamageCause
-            EntityInteractEvent EntityListener EntityPortalEnterEvent
+            EntityInteractEvent EntityPortalEnterEvent
             EntityRegainHealthEvent EntityShootBowEvent EntityTameEvent
             ;EntityTargetEvent EntityTeleportEvent ExplosionPrimeEvent
             EntityTargetEvent ExplosionPrimeEvent
             FoodLevelChangeEvent ItemDespawnEvent ItemSpawnEvent PigZapEvent
             PlayerDeathEvent PotionSplashEvent ProjectileHitEvent
-            SheepDyeWoolEvent SheepRegrowWoolEvent SlimeSplitEvent]))
+            SheepDyeWoolEvent SheepRegrowWoolEvent SlimeSplitEvent])
+  (:require clj-http.client))
 
 (defn broadcast [& strs]
   (.broadcastMessage (Bukkit/getServer) (apply str strs)))
 
-(defn entity-target-event* [evt]
+(defn entity-target-event [evt]
   (when (instance? Creeper (.getEntity evt))
-    (broadcast "Takumi is watching " (.getDisplayName (.getTarget evt)))))
+    (broadcast "Takumi is watching " (.. evt (getTarget) (getDisplayName)))))
 
-(defn entity-target-event []
-  (c/auto-proxy [org.bukkit.event.entity.EntityListener] []
-                (onEntityTarget [evt] (entity-target-event* evt))))
+(defonce swank* nil)
+(defn on-enable [plugin]
+  (when (nil? swank*)
+    (def swank* (swank.swank/start-repl 4006)))
+  (lingr "takumisamagamiteru plugin running..."))
 
-(def plugin-manager* (Bukkit/getPluginManager))
-(def plugin* (.getPlugin plugin-manager* "takumisamagamiteru"))
-
-(defn hehehe [f label]
-  (let [listener (f)]
-    (.registerEvent
-      plugin-manager*
-      (label c/event-types)
-      listener
-      (:Normal c/event-priorities)
-      plugin*)))
-
-(defn enable-plugin [plugin]
-  (hehehe entity-target-event :ENTITY_TARGET)
-  (c/log-info "takumisamagamiteru started"))
-
-(defn disable-plugin [plugin]
-  (c/log-info "takumisamagamiteru stopped"))
-
-(defn restart []
-  (.disablePlugin plugin-manager* plugin*)
-  (.enablePlugin plugin-manager* plugin*))
+;  (lingr "takumisamagamiteru plugin stopping...")
