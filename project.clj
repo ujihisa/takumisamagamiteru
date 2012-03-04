5a50bc32ad37843cac291d110ca9e8a873f2f6d4
ujihisa <ujihisa@gmail.com>
2012-03-04 00:02:00 -0800

    for 1.2

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject takumisamagamiteru "1.0.0-SNAPSHOT"
+(defproject takumisamagamiteru "2.0.0-SNAPSHOT"
   :description "A minecraft mod that alerts you if a creeper targets you!"
-  :dependencies []
-  :dev-dependencies [[org.bukkit/bukkit "1.1-R4"]
-                     [clj-minecraft "1.0.0-SNAPSHOT"]
-                     [org.clojure/clojure "1.3.0"]
-                     [org.clojure/tools.logging "0.2.3"]]
-  :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
-  :repositories {"spout-repo-snap" "http://repo.getspout.org/content/repositories/snapshots/"
-                 "spout-repo-rel" "http://repo.getspout.org/content/repositories/releases/"})
+  :dependencies [[clj-http "0.3.1"]
+                 [swank-clojure/swank-clojure "1.3.3"]
+                 [org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.2.3-R0"]
+                     [clj-minecraft "1.0.0-SNAPSHOT"]]
+  :javac-options {:destdir "classes/"}
+  :java-source-path "javasrc")
